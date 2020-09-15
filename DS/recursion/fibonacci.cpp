15bc75168249e8f3b01df20831e53813369ff61a
nisha <nishay0300@gmail.com>
2020-09-15 11:30:43 +0530

    Add files via upload

---
+++ DS/recursion/fibonacci.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int fib(int n)
+{
+    if (n == 1 || n==2)
+    {
+        return 1;
+    }
+    else
+    {
+        return fib(n-1) + fib(n-2);
+    }
+    
+}
+int main()
+{  
+    int N;
+    cin >> N;
+
+    int sum = fib(N);
+
+    cout << sum;
+   
+    return 0;
+}
